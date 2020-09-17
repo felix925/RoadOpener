eeb79432692c6a6bd9452d05a3989eb6780aac29
felix925 <45941545+felix925@users.noreply.github.com>
2020-09-17 15:36:03 +0900

    update

--- app/src/main/java/jp/making/felix/roadopener/util/OnClickCallBackListener.kt
+++
@@ -1,5 +0,0 @@
-package jp.making.felix.roadopener.util
-
-interface OnClickCallBackListener {
-    fun onClickBack()
-}
--- app/src/main/java/jp/making/felix/roadopener/util/Path.kt
+++
@@ -1,6 +0,0 @@
-package jp.making.felix.roadopener.util
-
-data class Path(
-    val title: String,
-    val isComplete: Boolean
-)
